3eb5cb59a2cb3cd9603006269bea0d86307eed9f
damon-daemon <17775525+damon-daemon@users.noreply.github.com>
2025-06-15 00:18:01 -0400

    add getbooks func

--- books.go
+++ books.go
@@ -6,6 +6,7 @@ type Book struct {
 	Title  string
 	Author string
 	Copies int
+	ID     string
 }
 
 var catalog = []Book{
@@ -13,11 +14,13 @@ var catalog = []Book{
 		Title:  "1984",
 		Author: "George Orwell",
 		Copies: 10,
+		ID:     "abc",
 	},
 	{
 		Title:  "Brave New World",
 		Author: "Aldous Huxley",
 		Copies: 5,
+		ID:     "def",
 	},
 }
 
@@ -27,4 +30,13 @@ func BookToString(book Book) string {
 
 func GetAllBooks() []Book {
 	return catalog
+}
+
+func GetBook(id string) (Book, bool) {
+	for _, book := range catalog {
+		if book.ID == id {
+			return book, true
+		}
+	}
+	return Book{}, false
 }
--- books_test.go
+++ books_test.go
@@ -7,10 +7,12 @@ import (
 )
 
 func TestBookToString_FormatsBookInfoAsString(t *testing.T) {
+	t.Parallel()
 	input := books.Book{
 		Title:  "Homage to Catalonia",
 		Author: "George Orwell",
 		Copies: 15,
+		ID:     "xyz",
 	}
 	exp := "Homage to Catalonia by George Orwell (copies: 15)"
 	resp := books.BookToString(input)
@@ -20,21 +22,49 @@ func TestBookToString_FormatsBookInfoAsString(t *testing.T) {
 }
 
 func TestGetAllBooks_ReturnsAllBooks(t *testing.T) {
+	t.Parallel()
 	exp := []books.Book{
 		{
 			Title:  "1984",
 			Author: "George Orwell",
 			Copies: 10,
+			ID:     "abc",
 		},
 		{
 			Title:  "Brave New World",
 			Author: "Aldous Huxley",
 			Copies: 5,
+			ID:     "def",
 		},
 	}
 	resp := books.GetAllBooks()
 
 	if !slices.Equal(exp, resp) {
 		t.Fatalf("expected %#v but got %#v", exp, resp)
+	}
+}
+
+func TestGetBook_ReturnBook(t *testing.T) {
+	t.Parallel()
+	exp := books.Book{
+		ID:     "abc",
+		Title:  "1984",
+		Author: "George Orwell",
+		Copies: 10,
+	}
+	resp, ok := books.GetBook("abc")
+	if !ok {
+		t.Fatalf("book not found")
+	}
+	if exp != resp {
+		t.Fatalf("expected %#v but got %#v", exp, resp)
+	}
+}
+
+func TestGetBook_ReturnFalseWhenBookNotFoun(t *testing.T) {
+	t.Parallel()
+	_, ok := books.GetBook("does-not-exist")
+	if ok {
+		t.Fatal("want false for nonexistent ID, got true")
 	}
 }
